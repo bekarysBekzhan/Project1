efb094eaff26abb6a0613d5569aecd693e854bfa
bekarysBekzhan <100556218+bekarysBekzhan@users.noreply.github.com>
2022-02-28 19:10:44 +0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Test extends Question {
-	
+    private String[] options;
+    private int numOfOptions;
+    private ArrayList<String> labels;
+
+    Test(){}
+
+    public String getOptionAt(int opt) {
+        return this.options[opt];
+    }
+
+    public void setOptions(String[] options){
+        this.options = options;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "options=" + Arrays.toString(options) +
+                ", numOfOptions=" + numOfOptions +
+                ", labels=" + labels +
+                '}';
+    }
 }
