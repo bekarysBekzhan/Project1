345c241bde3717897945c6818dc0e0a09ce2dcc6
bekarysBekzhan <100556218+bekarysBekzhan@users.noreply.github.com>
2022-02-28 18:48:41 +0600

    Update QuizMaker.java

--- QuizMaker.java
+++ QuizMaker.java
@@ -1,1 +1,43 @@
-public
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Quiz {
+    private String name;
+    private ArrayList<String> questions;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    Quiz(){}
+
+    public static Quiz loadFromFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner in = new Scanner(file);
+
+
+
+    }
+
+
+
+    public void addQuestion(Question question){
+
+    }
+
+    public void start(){
+
+    }
+
+}
+public class QuizMaker{
+    public static void main(String[] args) {
+        
+    }
+}
